9a11c8bcd6a33c8845a6abedf8a57fb5d48023b4
n00k1e <plotnikoff1488@gmail.com>
2022-12-09 19:07:56 +0200

    design changes

---
+++ project-app/src/components/darkswitch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DarkMode = (props) => {
+    return(
+        <div> Dark Mode </div>
+    )
+}
+
+export default DarkMode;
--- project-app/src/components/navbar.js
+++ project-app/src/components/navbar.js
@@ -4,9 +4,11 @@ import React from "react";
 const Navbar = () => {
     return(
         <div>
-            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./gaming"> Gaming </a>
-            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./news">  TechNews  </a>
-            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./score">  Score </a>
+            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./gaming"> Games </a>
+            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./news">  News  </a>
+            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./anime"> Anime </a>
+            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./hardware"> Hardware </a>
+            <a style = {{ marginLeft: '40px', color: 'white' }} className="space" href="./topone"> Top 100 </a>
         </div>
     );
 };
