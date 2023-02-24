cc3a1e4a2fcc0e94a12dcf78a41d3e1c7da3a982
n00k1e <plotnikoff1488@gmail.com>
2023-02-24 23:07:55 +0200

    changed navbar

--- project-app/src/App.js
+++ project-app/src/App.js
@@ -1,8 +1,8 @@
 // import './css/button.css';
-// import './css/navbar.css';
-// import './css/footer.css';
-import { BrowserRouter, Route, Routes, Link} from "react-router-dom";
-import Clock from "./components/clock";
+import './css/navbar.css';
+import './css/footer.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+//import Clock from "./components/clock";
 import Navbar from "./components/navbar";
 import Games  from "./components/gaming";
 import News  from "./components/news";
@@ -10,38 +10,25 @@ import Anime  from "./components/anime";
 import Hardware  from "./components/hardware";
 import Meta  from "./components/meta";
 import Footer from "./components/footer";
-import Button from './components/button';
+// import Button from './components/button';
 
 
 export default function App(){
   return(
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route path="games" component={ Games }/>
-          <Route path="news" component={ News } />
-          <Route path="hardware" component={ Hardware } />
-          <Route path="anime" component={ Anime }/>
-          <Route path="meta" component={ Meta }/>
-        </Route>
-      </Routes>
-      <div className="navigate">
-        <div className="title">
-          <span className="maintext"> 
-          <Link to={"/"} > MAKEDIE </Link> 
-          </span>
-        </div>
-        <div className="timer">
-          <Clock />
-        </div>
-        <div className='button'>
-          <Button />
-        </div>
+      <div className="navbar">
+         <Navbar />
       </div>
-      <div className='borders'></div>
-      <div className="footer">
+      <div className='footer'>
         <Footer />
       </div>
+      <Routes>
+        <Route path="/games" component={ Games }/>
+        <Route path="/news" component={ News } />
+        <Route path="/hardware" component={ Hardware } />
+        <Route path="/anime" component={ Anime }/>
+        <Route path="/meta" component={ Meta }/>
+      </Routes>
     </BrowserRouter>
   )
 };
